029dbf4369762f15e28799767a46c2a6dc86b420
teranishikawa <nishikawa@cdn-tera.co.jp>
2019-09-16 19:23:11 +0900

    ヘッダがかぶっていたので、テンプレから削除したけど合ってるかな

--- page-inquiry.php
+++ page-inquiry.php
@@ -5,37 +5,11 @@
 <?php get_header(); ?>
 		<main>
 			<div class="contents">
-				<div class="main-visual">
-					<div class="inner">
-						<figure><img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact/main_visual_pc.jpg" alt="お問い合わせ" class="js-switch"></figure>
-						<h2 data-aos="fade-left">お問い合わせ<span class="eng">CONTACT US</span></h2>
-					</div>
-				</div>
-
-				<section class="wide topics-box">
-					<div class="inner">
-
-<div class="feature" data-aos="fade-up">
-	<div class="width-fixed">
-
-<h3 class="title"><span class="eng line">PHONE</span>お電話でのお問い合わせ</h3>
-<p class="attention">お電話でのお問い合わせ、ご意見、ご要望は以下の電話番号までお願いいたします。</p>
-<div class="tel">
-<span><i class="fas fa-phone"></i>0000-00-0000</span>
-【受付時間】平日8:00 ～ 17:00【休業日】土・日・祝
-</div>
-
-<h3 class="title"><span class="eng line">FORM</span>フォームからのお問い合わせ</h3>
-
+	
 <?php if(have_posts()): while(have_posts()):the_post(); ?>
 <?php the_content(); ?>
 <?php endwhile; endif; ?>
 
-	</div>
-</div>
-
-					</div>
-				</section>
 
 				<section class="pagelink-area">
 					<div class="width-fixed">
